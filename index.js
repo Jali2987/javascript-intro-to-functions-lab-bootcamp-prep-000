edc9a7591ad23e6ae90b5de0e737eaab72928685
jalissa.nugent@yahoo.com <jalissa.nugent@yahoo.com>
2018-06-17 20:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ return string.toLowerCase()
 }
 
 
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})
+
+function logShout(string) {
+  console.log() string.andCallThrough
+}
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
 
 
 
